39f881c31a016a9099de502ea59bbab094055032
Kevin Soffa <84532185+KevinSoffa@users.noreply.github.com>
2022-01-28 00:27:31 -0300

    Segundo commit
    Criando Class Base Views (Mais funcional)
    Criando página de detalhes filmes

--- filme/urls.py
+++ filme/urls.py
@@ -1,10 +1,13 @@
 # url - views - template
 
-from .views import homepage, homefilmes
+from .views import Homepage, Homefilmes, Detalhesfilme
 from django.urls import path, include
 
 
+app_name = 'filme'
+
 urlpatterns = [
-    path('', homepage),
-    path('filmes/', homefilmes),
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name="detalhesfilme"),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,13 +1,21 @@
+from django.views.generic import TemplateView, ListView, DetailView
 from django.shortcuts import render
 from .models import Filme
 
 # Create your views here.
-def homepage(request):
-    return render(request, 'homepage.html')
+#def homepage(request):
+#return render(request, 'homepage.html')
 
 
-def homefilmes(request):
-    context = {}
-    lista_filmes = Filme.objects.all()
-    context['lista_filmes'] = lista_filmes
-    return render(request, 'homefilmes.html', context)
+class Homepage(TemplateView):
+    template_name= 'homepage.html'
+
+
+class Homefilmes(ListView):
+    template_name = 'homefilmes.html'
+    model = Filme
+    # objecy_list
+
+class Detalhesfilme(DetailView):
+    template_name = 'detalhesfilme.html'
+    model = Filme
