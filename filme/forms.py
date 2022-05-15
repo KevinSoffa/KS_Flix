b6c3248a930611fa8bcde2912472468c4a0b0589
Kevin Soffa <84532185+KevinSoffa@users.noreply.github.com>
2022-05-15 20:35:12 -0300

    Corrigindo cadastramento de novos usuarios

--- filme/forms.py
+++ filme/forms.py
@@ -5,6 +5,9 @@ from django import forms
 
 class CriarContaForm(UserCreationForm):
     email = forms.EmailField()
+    class Meta:
+        model = Usuario
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class FormHomePage(forms.Form):
